79baaee1ada221e149e39784befcb29cea00d2fe
vadim <koozyy@bigmir.net>
2016-08-03 13:26:37 +0300

    first

---
+++ bee_task (1).sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Авг 03 2016 г., 12:25
+-- Версия сервера: 10.1.13-MariaDB
+-- Версия PHP: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `bee_task`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `text` text NOT NULL,
+  `edit` int(11) NOT NULL DEFAULT '0',
+  `date` varchar(111) NOT NULL,
+  `confirm` tinyint(10) NOT NULL DEFAULT '0',
+  `img` varchar(150) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `name`, `email`, `text`, `edit`, `date`, `confirm`, `img`) VALUES
+(131, 'admin', 'kozak13vadim13@bigmir.net', 'text', 0, '2016-8-3', 0, '189320312'),
+(130, 'ivan', 'koozyy@bigmir.net', 'test', 0, '2016-8-3', 0, '660117510'),
+(128, 'ÑƒÐºÐµÑƒÐº', 'koozyy@bigmir.net', 'ÑƒÐºÐµÑƒÐºÐµÑƒÐº', 0, '2016-8-3', 1, '990144'),
+(129, 'Vadim', 'koozyy@bigmir.net', 'Ñ‚ÐµÐºÑÑ‚', 0, '2016-8-3', 0, '912218261');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` int(100) NOT NULL,
+  `role` varchar(111) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'vadim', 'koozyy@bigmir.net', 123, 'admin');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
